78ae2a236dcea8cca2e7352a1a8ad146a7f00ee1
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-17 21:00:58 +0530

    Create Main Frame and main loop

--- Main.py
+++ Main.py
@@ -1,6 +1,18 @@
-from tkinter import*
 import random
 import time;
 import datetime
 import tkinter.messagebox
+from tkinter import*
 
+root = Tk()
+root.geometry("1350x750+0+0")
+root.title("Restaurant Management Systems")
+root.configure(background = 'Cadet Blue')
+
+Tops = Frame(root, bg='Cadet Blue', bd=20, pady=5 relief=RIDGE)
+Tops.pack(side=TOP)
+
+MenuFrame = Frame(root, bg='Cadet Blue', bd=10, pady=5 relief=RIDGE)
+MenuFrame.pack(side=LEFT)
+
+root.mainloop()

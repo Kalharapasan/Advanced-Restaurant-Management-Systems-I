7a32c8ffc8b98da09d6ac063886d5d27cc909749
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-17 21:06:55 +0530

    Create Main MenuFrame,ReceiptCal_F,Buttons_F in application

--- Main.py
+++ Main.py
@@ -15,19 +15,19 @@ Tops.pack(side=TOP)
 lblTitle = Label(Tops, font=('arial', 60, 'bold'), text="Restaurant Management Systems", bd=21, bg='Cadet Blue', fg='Cornsilk', justify=CENTER)
 lblTitle.grid(row=0, column=0)
 
-MenuFrame = Frame(root, bg='Cadet Blue', bd=10, relief=RIDGE)
-MenuFrame.pack(side=LEFT)
-
 ReceiptCal_F = Frame(root, bg='Powder Blue', bd=10, relief=RIDGE)
 ReceiptCal_F.pack(side=RIGHT)
 
 Buttons_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=3, relief=RIDGE)
 Buttons_F.pack(side=BOTTOM)
 
-Cal_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=3, relief=RIDGE)
+Cal_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=6, relief=RIDGE)
 Cal_F.pack(side=TOP)
 
-Receipt_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=3, relief=RIDGE)
+Receipt_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=4, relief=RIDGE)
 Receipt_F.pack(side=BOTTOM)
+
+MenuFrame = Frame(root, bg='Cadet Blue', bd=10, relief=RIDGE)
+MenuFrame.pack(side=LEFT)
 
 root.mainloop()

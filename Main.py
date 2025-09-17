7842b4da3480d9f18f9bf8ce160e47e37854f60a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-09-17 21:04:05 +0530

    Create Main MenuFrame,ReceiptCal_F,Buttons_F in application

--- Main.py
+++ Main.py
@@ -9,10 +9,25 @@ root.geometry("1350x750+0+0")
 root.title("Restaurant Management Systems")
 root.configure(background = 'Cadet Blue')
 
-Tops = Frame(root, bg='Cadet Blue', bd=20, pady=5 relief=RIDGE)
+Tops = Frame(root, bg='Cadet Blue', bd=20, pady=5, relief=RIDGE)
 Tops.pack(side=TOP)
 
-MenuFrame = Frame(root, bg='Cadet Blue', bd=10, pady=5 relief=RIDGE)
+lblTitle = Label(Tops, font=('arial', 60, 'bold'), text="Restaurant Management Systems", bd=21, bg='Cadet Blue', fg='Cornsilk', justify=CENTER)
+lblTitle.grid(row=0, column=0)
+
+MenuFrame = Frame(root, bg='Cadet Blue', bd=10, relief=RIDGE)
 MenuFrame.pack(side=LEFT)
+
+ReceiptCal_F = Frame(root, bg='Powder Blue', bd=10, relief=RIDGE)
+ReceiptCal_F.pack(side=RIGHT)
+
+Buttons_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=3, relief=RIDGE)
+Buttons_F.pack(side=BOTTOM)
+
+Cal_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=3, relief=RIDGE)
+Cal_F.pack(side=TOP)
+
+Receipt_F=Frame(ReceiptCal_F, bg='Powder Blue', bd=3, relief=RIDGE)
+Receipt_F.pack(side=BOTTOM)
 
 root.mainloop()
